e1160837bee4807fd21351adb96cd0e0691b9513
jonaspereirar <jonaspereirar@gmail.com>
2023-03-08 07:15:58 +0000

    add dist base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gym-typescript",
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
