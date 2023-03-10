c0779d0ec79294f8b0aadd5f5a684c4365d5a1df
jonaspereirar <jonaspereirar@gmail.com>
2023-03-10 20:52:42 +0000

    add ide button

--- src/scenes/regulamentos/index.tsx
+++ src/scenes/regulamentos/index.tsx
@@ -120,8 +120,8 @@ const Regulamentos = ({ setSelectedPage }: Props) => {
             <motion.div
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: true, amount: 0.2 }}
-              transition={{ delay: 0.1, duration: 0.2 }}
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
               variants={{
                 hidden: { opacity: 0, x: 50 },
                 visible: { opacity: 1, x: 0 },
@@ -209,14 +209,14 @@ const Regulamentos = ({ setSelectedPage }: Props) => {
               <p className="my-5">Inscrição válidas após o envio do comprovativo de Transferências bancárias</p>
             </motion.div>
 
-            {/* BUTTON 
+            {/* BUTTON */}
             <div className="relative mt-16">
               <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
                 {/* <ActionButton link="Home" setSelectedPage={setSelectedPage}>
                   Inscrever-se
                 </ActionButton> */}
               </div>
-            </div> */} 
+            </div>
           </div>
         </div>
       </motion.div>
