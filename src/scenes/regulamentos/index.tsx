83d4b25121819c1fecdcad18ba8c626da92af1a2
Jonas Pereira <48494302+jonaspereirar@users.noreply.github.com>
2023-03-10 06:44:29 +0000

    Update index.tsx

--- src/scenes/regulamentos/index.tsx
+++ src/scenes/regulamentos/index.tsx
@@ -121,8 +121,8 @@ const Regulamentos = ({ setSelectedPage }: Props) => {
             <motion.div
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ delay: 0.2, duration: 0.5 }}
+              viewport={{ once: true, amount: 0.2 }}
+              transition={{ delay: 0.1, duration: 0.2 }}
               variants={{
                 hidden: { opacity: 0, x: 50 },
                 visible: { opacity: 1, x: 0 },
