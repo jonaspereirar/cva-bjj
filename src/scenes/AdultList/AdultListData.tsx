23e3f8ff91acbda3912f37dd910c6d7e28221f8f
Jonas Pereira <48494302+jonaspereirar@users.noreply.github.com>
2023-04-24 23:57:58 +0100

    Update AdultListData.tsx

--- src/scenes/AdultList/AdultListData.tsx
+++ src/scenes/AdultList/AdultListData.tsx
@@ -113,7 +113,7 @@ export const Atletas: DadosAdulto[] = [
   { nome: "João Dias", faixa: "Azul", peso: "Leve", categoria: "Master 1", equipe: "Icon jiu-jitsu team", sexo: "Masculino"  },  
   { nome: "Daniele Réus", faixa: "Azul", peso: "Pena", categoria: "Adulto", equipe: "Família Marola (Faro)", sexo: "Feminino"  },
   { nome: "Miguel  Silvino", faixa: "Preta", peso: "Leve", categoria: "Master 4", equipe: "Clone bjj", sexo: "Masculino"  },
-  { nome: "Elias Leal", faixa: "Azul", peso: "Super-Pesado", categoria: "Master 4", equipe: "Clone bjj", sexo: "Masculino"  },
+  { nome: "Elias Leal", faixa: "Azul", peso: "Super-Pesado", categoria: "Master 2", equipe: "Clone bjj", sexo: "Masculino"  },
   { nome: "Ericles godoi", faixa: "Azul", peso: "Super-Pesado", categoria: "Master 4", equipe: "Clone bjj", sexo: "Masculino"  },
   { nome: "Carlos Sequeira Guerreiro ", faixa: "Marron", peso: "Pesado", categoria: "Master 4", equipe: "CVA", sexo: "Masculino"  },
   { nome: "Paulo Júlio ", faixa: "Marron", peso: "Médio", categoria: "Master 4", equipe: "Clone bjj", sexo: "Masculino"  },
