a6b1a5fc5ad63f7dc511512ec4ea915eef53847b
jonaspereirar <jonasprodrigues@gmail.com>
2024-05-08 15:15:24 +0100

    retirada lista de inscrito App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import AdultList from "@/scenes/AdultList";
+// import AdultList from "@/scenes/AdultList";
+// import InfantList from "./scenes/InfantList";
 import Contacto from "@/scenes/contacto";
 import Footer from "@/scenes/footer";
 import Home from "@/scenes/home";
@@ -6,7 +7,6 @@ import Navbar from "@/scenes/navbar";
 import Regulamentos from "@/scenes/regulamentos";
 import { SelectedPage } from "@/shared/types";
 import { useEffect, useState } from "react";
-import InfantList from "./scenes/InfantList";
 // import Lutas from "./scenes/Lutas";
 
 function App() {
@@ -36,8 +36,8 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Regulamentos setSelectedPage={setSelectedPage} />
-      <AdultList setSelectedPage={setSelectedPage} />
-      <InfantList setSelectedPage={setSelectedPage} />
+      {/* <AdultList setSelectedPage={setSelectedPage} />
+      <InfantList setSelectedPage={setSelectedPage} /> */}
       {/* <Lutas setSelectedPage={setSelectedPage} /> */}
       <Contacto setSelectedPage={setSelectedPage} />
       <Footer />
