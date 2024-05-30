5a6e4d478846e393a2ef7fcc752b744f74b5d13b
jonaspereirar <jonasprodrigues@gmail.com>
2024-05-29 21:27:32 -0300

    feat add retirando botões dowload pdf lutas e cronograma

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -4,17 +4,10 @@ import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.gif";
 import useMediaQuery from "@/hooks/useMediaQuery";
-import { ButtonCronograma } from "@/shared/ButtonCronogramProps";
-import { BotaoDownload } from "@/shared/ButtonDownloadProps";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 
-import adultoPDF from "@/scenes/Lutas/adulto.pdf";
-import cronogramaPDF from "@/scenes/Lutas/cronograma.pdf";
 import femininosPDF from "@/scenes/Lutas/femininos.pdf";
-import juvenilPDF from "@/scenes/Lutas/juvenil.pdf";
-import infantilPDF from "@/scenes/Lutas/kids.pdf";
-import mastersPDF from "@/scenes/Lutas/masters.pdf";
 
 
 
@@ -116,13 +109,15 @@ const Home = ({ setSelectedPage }: Props) => {
 
   </motion.div>
   
+  {/* Botões de Download Cronograma */}
+{/* 
   <div className="flex flex-col items-center mb-8">
     <p className="text-white text-xl">Cronograma</p>
     <ButtonCronograma arquivoPath={cronogramaPDF} nomeArquivo="Cronograma" />
-  </div>
+  </div> */}
 
 {/* Botões de Download */}
-<motion.div
+{/* <motion.div
   className="mt-8 flex flex-wrap justify-center md:justify-start gap-8"
   initial="hidden"
   whileInView="visible"
@@ -159,7 +154,7 @@ const Home = ({ setSelectedPage }: Props) => {
     <p className="text-white text-xl">Chaves Masters</p>
     <BotaoDownload arquivoPath={mastersPDF} nomeArquivo="Chaves_masters" />
   </div>
-</motion.div>
+</motion.div> */}
 
 
         </div>
