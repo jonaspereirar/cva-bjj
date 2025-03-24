85d39b22fe8d3a95ad5f0b6163bcb34e1650d5c8
jonaspereirar <jonasprodrigues@gmail.com>
2025-03-24 23:53:47 +0000

    feat add list update 24/03

--- src/scenes/AdultList/AdultListData.tsx
+++ src/scenes/AdultList/AdultListData.tsx
@@ -6,5 +6,8 @@ export const Atletas: DadosAdulto[] = [
     { nome: "ANDRÉ ALMEIDA", equipe: "SAMPAIO BJJ", sexo: "Masculino", categoria: "Master 3", peso: "Pesadíssimo", faixa: "Branca" },
     { nome: "Gabriel Piçarra", equipe: "Bear bones", sexo: "Masculino", categoria: "Adulto", peso: "Pena", faixa: "Azul" },
     { nome: "Roman", equipe: "Familia Marola Algarve", sexo: "Masculino", categoria: "Master 2", peso: "Médio", faixa: "Branca" },
-    { nome: "Helder Silva", equipe: "Academia Mente Blindada, Algueirao Mem Martins", sexo: "Masculino", categoria: "Adulto", peso: "Pesado", faixa: "Preta" }
+    { nome: "Helder Silva", equipe: "Academia Mente Blindada, Algueirao Mem Martins", sexo: "Masculino", categoria: "Adulto", peso: "Pesado", faixa: "Preta" },
+    { nome: "Rodrigo Silva", equipe: "CVABJJ", sexo: "Masculino", categoria: "Adulto", peso: "Pluma", faixa: "Marron" },
+    { nome: "Ismael Poeta", equipe: "Família Marola Algarve", sexo: "Masculino", categoria: "Master 3", peso: "Médio", faixa: "Azul" },
+    { nome: "Diane Grossot", equipe: "Bear bones", sexo: "Feminino", categoria: "Master 3", peso: "Médio", faixa: "Azul" }
 ];
