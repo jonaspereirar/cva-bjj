bf81023ea0a597035f32acea71a42cd20267bda7
jonaspereirar <jonasprodrigues@gmail.com>
2025-03-25 14:11:25 +0000

    feat add Pedro Satana

--- src/scenes/InfantList/InfantListData.tsx
+++ src/scenes/InfantList/InfantListData.tsx
@@ -4,6 +4,7 @@ export const Atletas: DadosInfatil[] =
 
 [
   { nome: "Miguel Pereira Pessoa", equipe: "CVABJJ", peso: 35, faixa: "Cinza/Branca", categoria: "Infanto-Juvenil" },
+  { nome: "Pedro Santana", equipe: "Old Dogs", peso: 64, faixa: "Azul", categoria: "Juvenil" },
 
   ]
   
